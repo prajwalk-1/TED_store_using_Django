06c8a553c3f2f287c135b476fbcfc24a9ea7a188
Prajwal Kanade <142607979+prajwalk-1@users.noreply.github.com>
2024-10-15 11:22:35 +0530

    Update serializers.py

--- product/serializers.py
+++ product/serializers.py
@@ -1,19 +1,20 @@
 from rest_framework import serializers
 from .models import Product, ProductDetails, Tag, Review, RelatedProduct
 
+# Serializer for Tag model to convert model instances to JSON format and validate input data
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = '__all__'
+        fields = '__all__'  
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = '__all__' 
 
 class ProductDetailsSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
+    product = ProductSerializer()  # Nesting ProductSerializer to include product details
 
     class Meta:
         model = ProductDetails
-        fields = '__all__'
+        fields = '__all__' 
