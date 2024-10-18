261ca5cacfe44662f278e3c493aa6a73fe2881e5
Prajwal Kanade <142607979+prajwalk-1@users.noreply.github.com>
2024-10-18 14:13:39 +0530

    Updated views.py

--- review/views.py
+++ review/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import ProductReview
-from .serializers import ProductReviewSerializer
+from .mels import ProductReview
+from .serializers import ProductReviewSerializerod
 
 class ProductReviewView(APIView):
     def get(self, request, product_id):
